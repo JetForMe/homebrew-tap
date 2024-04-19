aa86b227541829477b726b98c463f4319bb43184
Rick M <rmann@latencyzero.com>
2024-04-18 23:55:44 -0700

    Update raise3d-tool.rb

--- Formula/raise3d-tool.rb
+++ Formula/raise3d-tool.rb
@@ -1,4 +1,4 @@
-class Raise3dTools < Formula
+class Raise3dTool < Formula
   desc "A command line application for monitoring Raise3D 3D printers."
   homepage ""
   url "https://github.com/JetForMe/raise3d-swift/releases/download/v0.0.1/v0.0.1.tar.gz"
